dbe62830812c30ee283a0961b4d134a285b71094
dernewt <dernewt@gmail.com>
2023-04-20 21:48:24 -0400

    create settings plus poco, stash data files locally. Not sure if I'll use the URLs or just run off files.

---
+++ Bikes.Console/BikeSources.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Bikes.Console;
+
+public record BikeSources(
+    string[] TripFiles,
+    string[] TripUrls,
+    string[] StationFiles,
+    string[] StationUrls
+    )
+{
+    public BikeSources(IConfiguration config)
+        :this(Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>(), Array.Empty<string>())
+    {
+        var section = config.GetRequiredSection(nameof(BikeSources));
+        TripFiles = section.GetRequiredSection(nameof(TripFiles)).Get<string[]>()!;
+        TripUrls = section.GetRequiredSection(nameof(TripUrls)).Get<string[]>()!;
+        StationFiles = section.GetRequiredSection(nameof(StationFiles)).Get<string[]>()!;
+        StationUrls = section.GetRequiredSection(nameof(StationUrls)).Get<string[]>()!;
+    }
+}
