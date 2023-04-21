10ed7b8dc277620cdeec542c572ff71312d958c9
dernewt <dernewt@gmail.com>
2023-04-20 21:44:54 -0400

    add core data objects per csv, didn't do it verbatim tried to make it a more useful poco

---
+++ Bikes.Core/BikeRoute.cs
@@ -0,0 +1,12 @@
+﻿using UnitsNet;
+
+namespace Bikes.Core;
+
+public record BikeRoute(
+    DateTime Departure,
+    DateTime Return,
+    BikeStation From,
+    BikeStation To,
+    Length Distance,
+    TimeSpan Duration
+    );
---
+++ Bikes.Core/BikeStation.cs
@@ -0,0 +1,12 @@
+﻿namespace Bikes.Core;
+
+public record BikeStation(
+    uint Id,
+    string Name,
+    string Address,
+    string City,
+    string Operator,
+    uint Capacity,
+    string X, //ETRS-GK25
+    string Y //ETRS-GK25
+    );
