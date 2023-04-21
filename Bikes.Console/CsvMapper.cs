81f5ae2d39a5bc477d07688e541eaf3de60da169
dernewt <dernewt@gmail.com>
2023-04-20 21:50:22 -0400

    parse data csvs, not doing anything with the results but it looks right. Note the sneaky double for distance in meters and the missing stations.

---
+++ Bikes.Console/CsvMapper.cs
@@ -0,0 +1,57 @@
+﻿using Bikes.Core;
+using Sylvan.Data.Csv;
+using UnitsNet;
+
+namespace Bikes.Console;
+
+public static class CsvMapper
+{
+    public static IEnumerable<BikeRoute> GetRoutes(this CsvDataReader csv, Dictionary<uint, BikeStation> stationMap)
+    {
+        /*
+         * 0--------,1_____,2___________________,3_____________________,4________________,5__________________,6___________________,7_____________
+         * Departure,Return,Departure station id,Departure station name,Return station id,Return station name,Covered distance (m),Duration (sec.)
+         * 2021-05-31T23:57:25,2021-06-01T00:05:46,094,Laajalahden aukio,100,Teljäntie,2043,500
+         * */
+        var unknownStation = new BikeStation(0, "UNKNOWN", "", "", "", 0, "", "");
+        while (csv.Read())
+        {
+            var foundDeparture = stationMap.GetValueOrDefault(Convert.ToUInt32(csv.GetInt32(2)))
+                ?? unknownStation;
+            var foundReturn = stationMap.GetValueOrDefault(Convert.ToUInt32(csv.GetInt32(4)))
+                ?? unknownStation;
+            var row = new BikeRoute(
+                Departure: csv.GetDateTime(0),
+                Return: csv.GetDateTime(1),
+                From: foundDeparture,
+                To: foundReturn,
+                Distance: Length.FromMeters(csv.GetDouble(6)),
+                Duration: TimeSpan.FromSeconds(csv.GetInt64(7)));
+            yield return row;
+        }
+    }
+
+    public static IEnumerable<BikeStation> GetStations(this CsvDataReader csv)
+    {
+        /*
+         * seq,id,Name,SwdN,Name,Addres,SwdAdr,City----,SwdC,Operator--,Capacities,ETRS-GK25 (Helsinki) coordinates
+         * 0__,1_,2___,3___,4___,5_____,6_____,7_______,8___,9_________,10________,11,12
+         * FID,ID,Nimi,Namn,Name,Osoite,Adress,Kaupunki,Stad,Operaattor,Kapasiteet,x,y
+         * 1,501,Hanasaari,Hanaholmen,Hanasaari,Hanasaarenranta 1,Hanaholmsstranden 1,Espoo,Esbo,CityBike Finland,10,24.840319,60.16582
+         * */
+        while (csv.Read())
+        {
+            var row = new BikeStation(
+                Id: Convert.ToUInt32(csv.GetInt32(1)),
+                Name: csv.GetString(2),
+                Address: csv.GetString(5),
+                City: csv.GetString(7),
+                Operator: csv.GetString(9),
+                Capacity: Convert.ToUInt32(csv.GetInt32(10)),
+                X: csv.GetString(11),
+                Y: csv.GetString(12)
+                );
+            yield return row;
+        }
+    }
+}
--- Bikes.Console/Program.cs
+++ Bikes.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Sylvan.Data.Csv;
+using Bikes.Console;
+
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(serviceCollection => serviceCollection.AddScoped<BikeSources>())
+    .Build();
+
+var data = host.Services.GetRequiredService<BikeSources>();
+
+using var stationCsv = CsvDataReader.Create(data.StationFiles[0]);
+var stations = stationCsv.GetStations()
+    .ToDictionary(s=>s.Id, s=>s);
+
+using var routeCsv = CsvDataReader.Create(data.TripFiles[0]);
+var routes = routeCsv.GetRoutes(stations).ToArray();
+
+return 0;
